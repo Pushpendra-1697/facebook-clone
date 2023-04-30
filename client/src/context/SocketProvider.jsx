331bbdc1aa5b3dd91a1cb78764c4484471f9dfbc
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2023-04-30 18:38:36 +0530

    added

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -1,14 +1,13 @@
 import React, { createContext, useContext, useMemo } from "react";
 import { io } from 'socket.io-client';
 const SocketContext = createContext(null);
-
 export const useSocket = () => {
     const socket = useContext(SocketContext);
     return socket;
 };
 
 export const SocketProvider = (props) => {
-    const socket = useMemo(() => io('localhost:8000'), []);
+    const socket = useMemo(() => io('https://media-z88u.onrender.com', { port: 8000 }), []); //use localhost
     return (
         <SocketContext.Provider value={socket}>
             {props.children}
